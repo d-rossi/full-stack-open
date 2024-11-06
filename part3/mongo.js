058a388a38c1b6121dd4fa39ff8672f6fe91718e
d-rossi <david.rossi101@gmail.com>
2024-11-06 13:01:24 -0500

    exercise 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const PASSWORD = process.argv[2]
+
+const URI = `mongodb+srv://fullstackopen:${PASSWORD}@freecluster.z6kf8.mongodb.net/?retryWrites=true&w=majority&appName=FreeCluster`
+
+mongoose.connect(URI)
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const personSchema = new mongoose.Schema({
+    "name": String, 
+    "number": String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (!name || !number) {
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        "name": name,
+        "number": number
+    })
+    
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
