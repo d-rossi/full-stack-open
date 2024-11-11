eff1431637c2ab805e019c3cfe14754c1d115788
d-rossi <david.rossi101@gmail.com>
2024-11-11 16:51:04 -0500

    exercise 4.6 and 4.7

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -24,3 +24,58 @@ describe('total likes', () => {
         assert.strictEqual(listHelper.totalLikes(blogs), 19)
     })
 })
+
+describe('favorite blog', () => {
+    test('of empty list is undefined', () => {
+        assert.strictEqual(listHelper.favoriteBlog([]), undefined)
+    })
+
+    test('when list has only one blog that blog', () => {
+        const blog = {title: 'title1', likes: 5}
+        assert.deepStrictEqual(listHelper.favoriteBlog([blog]), blog)
+    })
+
+    test('when list has only many blogs the blog with most likes', () => {
+        const blogs = [{title: 'title1', likes: 5}, {title: 'title2', likes: 3}, {title: 'titl3', likes: 11}]
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[2])
+    })
+
+    test('when list has equal liked blogs one of the most liked', () => {
+        const blogs = [{title: 'title1', likes: 5}, {title: 'title2', likes: 3}, {title: 'titl3', likes: 5}]
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[0])
+    })
+})
+
+describe('most blogs', () => {
+    test('of empty list is undefined', () => {
+        assert.strictEqual(listHelper.mostBlogs([]), null)
+    })
+
+    test('when list has only one blog that blog', () => {
+        const blog = {title: 'title1', author: 'author1', likes: 5}
+        assert.deepStrictEqual(listHelper.mostBlogs([blog]), {author: 'author1', blogs: 1})
+    })
+
+    test('when list has many blogs the author that wrote the most blogs', () => {
+        const blogs = [{title: 'title1', author: 'author1', likes: 5}, {title: 'title2', author: 'author2', likes: 2}, {title: 'title3', author: 'author1', likes: 7}]
+        assert.deepStrictEqual(listHelper.mostBlogs(blogs), {author: 'author1', blogs: 2})
+    })
+})
+
+describe('most likes', () => {
+    test('of empty list is undefined', () => {
+        assert.strictEqual(listHelper.mostLikes([]), null)
+    })
+
+    test('when list has only one blog that blog', () => {
+        const blog = {title: 'title1', author: 'author1', likes: 5}
+        assert.deepStrictEqual(listHelper.mostLikes([blog]), {author: 'author1', likes: 5})
+    })
+
+    test('when list has many blogs the author that wrote the most blogs', () => {
+        const blogs = [{title: 'title1', author: 'author1', likes: 5}, {title: 'title2', author: 'author2', likes: 2}, {title: 'title3', author: 'author1', likes: 7}]
+        assert.deepStrictEqual(listHelper.mostLikes(blogs), {author: 'author1', likes: 12})
+    })
+})
+
+
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,7 +5,52 @@ const dummy = (blogs) => {
   const totalLikes = (blogs) => {
     return blogs.reduce((acc, currentVal) => acc + currentVal.likes, 0)
   }
-  
+
+  const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => { return blog.likes > max.likes ? blog : max }, blogs[0])
+  }
+
+  const mostBlogs = (blogs) => {
+    const authorToBlogCount = blogs.reduce((artistToBlogsCount, blog) => {
+        console.log(artistToBlogsCount)
+        const author = blog.author
+        if (artistToBlogsCount[`${author}`] == null) artistToBlogsCount[`${author}`] = 0
+        artistToBlogsCount[`${author}`]++
+        return artistToBlogsCount
+    }, {})
+
+    let maxCount = 0
+    let authorMostBlogs = ""
+    for (const [author, count] of Object.entries(authorToBlogCount)) { 
+        if (count > maxCount) { 
+            maxCount = count 
+            authorMostBlogs = author
+        } 
+    }
+    
+    return maxCount === 0 ? null : {author: authorMostBlogs, blogs: maxCount }
+  }
+
+  const mostLikes = (blogs) => {
+    const authorToLikesCount = blogs.reduce((authorToLikes, blog) => {
+        const author = blog.author
+        if (authorToLikes[`${author}`] == null) authorToLikes[`${author}`] = 0
+        authorToLikes[`${author}`] += blog.likes
+        return authorToLikes
+    }, {})
+
+    let maxCount = 0
+    let authorMostLikes = ""
+    for (const [author, count] of Object.entries(authorToLikesCount)) { 
+        if (count > maxCount) { 
+            maxCount = count 
+            authorMostLikes = author
+        } 
+    }
+    
+    return maxCount === 0 ? null : {author: authorMostLikes, likes: maxCount }
+  }
+
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
   }
