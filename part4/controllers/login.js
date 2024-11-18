d68d89becf9cdb1cc4f5bd61df0d56d112c705d8
d-rossi <david.rossi101@gmail.com>
2024-11-18 16:53:20 -0500

    exercise 4.17 to 4.22

---
+++ part4/controllers/login.js
@@ -0,0 +1,18 @@
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+loginRouter.post('/', async (request, response, next) => {
+    const {username, password} = request.body
+    const existingUser = await User.findOne({username: username})
+    const isPasswordCorrect = existingUser === null ? false : await bcrypt.compare(password, existingUser.passwordHash)
+    if (!(existingUser && isPasswordCorrect)) {
+        response.status(404).json({error: 'Username or password is invalid!'})
+    }
+
+    const token = jwt.sign({username: username, id: existingUser._id}, process.env.SECRET)
+    response.status(200).json({token, username: username, name: existingUser.name})
+})
+
+module.exports = loginRouter
---
+++ part4/utils/tokenExtractor.js
@@ -0,0 +1,11 @@
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token = authorization.replace('Bearer ', '')
+    } else {
+        request.token = ''
+    }
+    next()
+}
+
+module.exports = tokenExtractor
